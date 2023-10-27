eecccc83d8b865a0dd0c1a0469fd7b35c55e7618
AnaMaria27 <anamaria.cavasin@gmail.com>
2023-10-27 10:30:35 -0300

    versão final do user data e imagens documentacao

--- user_data.sh
+++ user_data.sh
@@ -2,16 +2,48 @@
 
 # Instalação do Docker
 sudo yum update -y
-sudo yum install -y docker
-sudo service docker start
-sudo usermod -aG docker ec2-user
+sudo yum install docker -y
+sudo systemctl start docker.service && systemctl enable docker.service
+usermod -aG docker ec2-user
 
-# Instalação do Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# estes comandos instalarão o Docker Compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
-# Inicia o Docker no boot
-chkconfig docker on
+# Montagem do EFS
+sudo yum install -y nfs-utils
+sudo mkdir /mnt/efs
+echo "fs-0e23f87bdba2182bd.efs.us-east-1.amazonaws.com:/ /mnt/efs nfs defaults 0 0" >> /etc/fstab
+mount -a
 
-# Reinicia a instância
-reboot
+# Instalação do MySQL 
+sudo yum install -y mysql
+
+# Criação do diretório Docker
+sudo mkdir /Docker
+
+# Criação do arquivo docker-compose.yml
+sudo tee /Docker/docker-compose.yml <<EOF
+version: '3.7'
+
+services:
+  wordpress:
+    image: wordpress
+    restart: always
+    ports:
+      - 80:80
+    environment:
+      WORDPRESS_DB_HOST: wordpress.c7djeexh6rth.us-east-1.rds.amazonaws.com
+      WORDPRESS_DB_NAME: wordpress
+      WORDPRESS_DB_USER: admin
+      WORDPRESS_DB_PASSWORD: wordpress
+    volumes: 
+      - /mnt/efs:/var/www/html
+EOF
+
+# Navega para o diretório Docker
+cd /Docker
+
+# Inicializa os contêineres Docker com o Docker Compose
+docker-compose up -d
+
