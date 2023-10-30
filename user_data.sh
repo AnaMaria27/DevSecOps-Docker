7b80206e38683ca5d491ca7b298c9388843aaca5
AnaMaria27 <anamaria.cavasin@gmail.com>
2023-10-30 11:05:55 -0300

    Versão final da documentação e do user_data

--- user_data.sh
+++ user_data.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-# Instalação do Docker
 sudo yum update -y
 sudo yum install docker -y
 sudo systemctl start docker.service && systemctl enable docker.service
 usermod -aG docker ec2-user
 
-# estes comandos instalarão o Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# Montagem do EFS
 sudo yum install -y nfs-utils
 sudo mkdir /mnt/efs
 echo "fs-0e23f87bdba2182bd.efs.us-east-1.amazonaws.com:/ /mnt/efs nfs defaults 0 0" >> /etc/fstab
 mount -a
 
-# Instalação do MySQL 
 sudo yum install -y mysql
 
-# Criação do diretório Docker
 sudo mkdir /Docker
 
-# Criação do arquivo docker-compose.yml
 sudo tee /Docker/docker-compose.yml <<EOF
 version: '3.7'
 
@@ -41,9 +35,7 @@ services:
       - /mnt/efs:/var/www/html
 EOF
 
-# Navega para o diretório Docker
 cd /Docker
 
-# Inicializa os contêineres Docker com o Docker Compose
 docker-compose up -d
 
